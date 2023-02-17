de86156de8d73c99764c13f718ca7c760621bc2d
Balázs Kovács <balazs.kovacs@talokuntoon.fi>
2023-02-17 09:25:55 +0200

    Strongly typed api request fix

--- src/pages/api/messages/index.ts
+++ src/pages/api/messages/index.ts
@@ -1,6 +1,7 @@
+import { NextApiRequest, NextApiResponse } from 'next';
 import client from '../../../../lib/db'
 
-const endpoint = async function(request, result) {
+const endpoint = async function(request: NextApiRequest, result: NextApiResponse) {
   try {
     const queryResult = await client.query('SELECT * FROM messages LIMIT 20');
     result.status(200).send(queryResult.rows)
